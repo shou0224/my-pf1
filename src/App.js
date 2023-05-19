a7462e5e40900e9d1639eb96a154ded4b34afe44
shou0224 <sugawarashou2@gmail.com>
2023-05-19 16:15:16 +0900

    サイトタイトル追加

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+import './App.css';
+import testFnc from './app.ts'
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant='h1'>菅原祥のポートフォリオ</Typography>
+      {/* <div>{testFnc()}</div> */}
+    </div>
+  );
+}
+
+export default App;
